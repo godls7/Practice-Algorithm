9786584da2852e04daa1fe972e32fe93f0e38d84
haeinyy <godls4402@gmail.com>
2021-06-27 18:28:24 +0900

    Update DFS/BFS study

--- 이것이코딩테스트다/구현/[8] 문자열 재정렬.py
+++ 이것이코딩테스트다/구현/[8] 문자열 재정렬.py
@@ -2,6 +2,9 @@
 #   1. 문자와 숫자 나눔
 #   2. 문자 오름차순정렬 / 숫자더해서 문자열로 합치기
 
+# !!!! 문자열 접근시, 문자열이 계속 바뀌게 되면 인덱스 접근 주의!!
+# 값을 다른 변수에 copy 해서 사용해야함!!
+
 # time complexity :
 ##########################################################
 # 입력
@@ -14,6 +17,7 @@
 # 출력
 # ADDIJJJKKLSS20
 ##########################################################
+import copy
 
 # 두번째 예시 안됨...
 string = list(input())
@@ -21,12 +25,17 @@ string = list(input())
 dight = str([0,1,2,3,4,5,6,7,8,9])
 number = 0
 
-for i in string:
-    # print(i)
+copy_s = copy.deepcopy(string)
+
+# print(string)
+
+for i in copy_s:
+    # print('문자',i, end=' ')
     if i in dight:
-        # print(i)
+        # print('숫자',i)
         number += int(i)
         string.remove(i)
+        print(string)
 
 result = ''.join(sorted(string)) + str(number)
 print(result)
--- 이것이코딩테스트다/구현/[9] 문자열 압축.py
+++ 이것이코딩테스트다/구현/[9] 문자열 압축.py
@@ -21,9 +21,9 @@ def solution(s):
     # 전체 길이에 반만큼 반복하면..
     for i in range(1, (string_len // 2 + 1)):
         part_string = s[0:i]  # 처음 부분 문자열은 이미 저장..?
-        print()
-        print('문자열 개수', i)
-        print('part :', part_string)
+        # print()
+        # print('문자열 개수', i)
+        # print('part :', part_string)
 
         # 같은 문자열 개수 세기
         count = 1
@@ -31,7 +31,7 @@ def solution(s):
 
         # 두번째 문자열 파트 부터 문자열 끝까지 i 간격으로 비교..?
         for j in range(i, string_len, i):
-            print('part-2', s[j:j + i])
+            # print('part-2', s[j:j + i])
             # 이전과 동일
             if part_string == s[j:j + i]: 
                 count += 1
@@ -44,6 +44,13 @@ def solution(s):
                 part_string = s[j:j+i]
                 count = 1 # 초기화
         
-    return min(answer, len(compress))
+        if count != 1:
+            compress = compress + str(count) + part_string
+        else:
+            compress += part_string
+        
+        answer = min(answer, len(compress))
+
+    return answer
 
 print(solution("ababcdcdababcdcd"))
