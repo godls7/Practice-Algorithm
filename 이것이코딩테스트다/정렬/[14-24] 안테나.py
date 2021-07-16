1e10d41319198e6da0c75e63a044bc9830a9a74f
haeinyy <godls4402@gmail.com>
2021-07-17 00:43:10 +0900

    Update [14-24]

--- 이것이코딩테스트다/정렬/[14-24] 안테나.py
+++ 이것이코딩테스트다/정렬/[14-24] 안테나.py
@@ -4,32 +4,23 @@
 # time complexity :
 ##########################################################
 # 입력
-# 12
-# Junkyu 50 60 100
-# Sangkeun 80 60 50
-# Sunyoung 80 70 100
-# Soong 50 60 90
-# Haebin 50 60 100
-# Kangsoo 60 80 100
-# Donghyuk 80 60 100
-# Sei 70 70 70
-# Wonseob 70 70 90
-# Sanghyun 70 70 90
-# nsj 80 80 80
-# Taewhan 50 60 90
+# 4
+# 5 1 7 9
 
 # 출력
+# 5
 ##########################################################
 
 n = int(input())
-students = []
+house = list(map(int, input().split()))
+
+all_dis = []
+poisition = 0
 
 for i in range(n):
-    student = input().split()
-    students.append(student)
+    dis = 0
+    for j in range(n):
+        dis += abs(house[j] - house[i])
+    all_dis.append(dis)
 
-students = sorted(students, key=lambda data:(-int(data[1]), int(data[2]), -int(data[3]), data[0]))
-
-
-for i in range(n):
-    print(students[i][0])
+print(house[all_dis.index(min(all_dis))])
