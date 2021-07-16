45b77eebae86c25a5efdd812b254d8d31abf0862
haeinyy <godls4402@gmail.com>
2021-07-16 21:53:08 +0900

    Update [6-2]

--- 이것이코딩테스트다/정렬/[6-2] 위에서아래로.py
+++ 이것이코딩테스트다/정렬/[6-2] 위에서아래로.py
@@ -4,5 +4,21 @@
 # time complexity :
 ##########################################################
 # 입력
+# 3
+# 15
+# 27
+# 12
 # 출력
+# 27 15 12
 ##########################################################
+
+n = int(input())
+
+result =[]
+for i in range(n):
+    result.append(int(input()))
+
+result = sorted(result, reverse=True)
+
+for i in result:
+    print(i, end=' ')
