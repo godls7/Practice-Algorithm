a66d4a21e21208f3aa0de2ea94f7029b1870d323
haeinyy <godls4402@gmail.com>
2021-07-17 17:54:43 +0900

    Update array study

---
+++ 이것이코딩테스트다/정렬/계수정렬.py
@@ -0,0 +1,13 @@
+# time complexity : O(n + k)
+
+arr = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8] # 기본 제공 배열
+
+count = [0] * (max(arr)+1)
+
+# 해당 인덱스 array에 넣어주기
+for i in range(len(arr)):
+    count[arr[i]] += 1
+
+for j in range(len(count)):
+    for k in range(count[j]):
+        print(k, end='')
